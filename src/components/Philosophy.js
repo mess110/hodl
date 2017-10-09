335a7e9d802204f344e9be809a34764ea30398f3
Cristian Mircea Messel <mess110@gmail.com>
2017-10-09 19:44:24 +0300

    update Philosophy

--- src/components/Philosophy.js
+++ src/components/Philosophy.js
@@ -6,7 +6,7 @@ import Checkbox from 'material-ui/Checkbox';
 class Philosophy extends Component {
   state = {
     open: process.env.NODE_ENV === 'production',
-    checked: false
+    checked: false,
   };
 
   updateCheck() {
@@ -37,8 +37,12 @@ class Philosophy extends Component {
           title="Hodl Wallet"
           actions={actions}
           modal={true}
+          autoScrollBodyContent={true}
           open={this.state.open}>
 
+          <p><b>This is experimental software, use at your own risk</b></p>
+          <p>Nothing is persisted</p>
+          <p>All network connections are blocked</p>
           <p>Think of this as a multi-coin paper wallet generator</p>
           <p>You need another wallet to send transactions</p>
           <p>I recommend holding your seed in something like CryptoSteel</p>
